8d0f4f7f0eb7bda8a9f44b3434eeb4d6b45bc115
Muhammad Ellbendl Satria <69744381+DecX-x@users.noreply.github.com>
2025-04-06 20:07:02 +0700

    Refactor llm.ts to initialize ChatOpenAI model with configuration settings

--- lib/llm.ts
+++ lib/llm.ts
@@ -3,9 +3,16 @@ import { config } from "dotenv";
 
 config();
 
-
+const apikey = process.env.DEEPINFRA_API_KEY;
 
 
 const model = new ChatOpenAI({
-    
+    model: "meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8",
+    apiKey: apikey,
+    temperature: 1.0,
+    configuration: {
+        baseURL: "https://api.deepinfra.com/v1/openai"
+    }
 })
+
+export default model;

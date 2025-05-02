3b3597ab4826c88be94aefd734bf0c644adea232
Muhammad Ellbendl Satria <69744381+DecX-x@users.noreply.github.com>
2025-05-02 19:40:18 +0700

    refactor: Simplify LoadingAnimation component by removing progress state and animation logic

--- components/analyzing/loading-animation.tsx
+++ components/analyzing/loading-animation.tsx
@@ -1,37 +1,8 @@
 "use client"
 
-import { useState, useEffect } from "react"
-
 export default function LoadingAnimation() {
-  const [progress, setProgress] = useState(0)
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setProgress((prev) => {
-        if (prev >= 100) {
-          clearInterval(interval)
-          return 100
-        }
-        return prev + 2
-      })
-    }, 100)
-
-    return () => clearInterval(interval)
-  }, [])
-
   return (
     <div className="space-y-4">
-      <div className="relative h-4 w-full bg-[#333333] rounded-full overflow-hidden">
-        <div
-          className="absolute top-0 left-0 h-full bg-gradient-to-r from-[#e67373] via-[#7ac97a] to-[#6bb7d3] transition-all duration-300 ease-out"
-          style={{ width: `${progress}%` }}
-        />
-      </div>
-
-      <div className="flex justify-between text-sm text-[#a0a0a0]">
-        <span>Analyzing</span>
-      </div>
-
       <div className="flex justify-center mt-8">
         <div className="relative w-24 h-24">
           <div className="absolute inset-0 rounded-full border-4 border-[#333333]"></div>
